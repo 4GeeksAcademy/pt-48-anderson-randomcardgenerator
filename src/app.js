3d656b70fc9d0cce7c776c03b0f4f6530293acc0
Anderson Gonzalez <141663667+AndyGH86@users.noreply.github.com>
2023-10-18 00:41:56 +0000

    casi listo

--- src/app.js
+++ src/app.js
@@ -1,5 +1,33 @@
 import "./style.css";
+window.onload = () => {
+  document.querySelector(".numeroCarta").innerHTML = generateRandomNumber();
+  document.querySelector(
+    ".pintaArriba",
+    ".pintaAbajo"
+  ).innerHTML = generateRandomPinta();
+};
 
-window.onload = function() {
-  console.log("todo okkk");
+let generateRandomNumber = () => {
+  const numbersCard = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let numberIndex = Math.floor(Math.random() * numbersCard.length);
+  return numbersCard[numberIndex];
+};
+let generateRandomPinta = () => {
+  const pintaCard = ["♦", "♥", "♠", "♣"];
+  let pintaIndex = Math.floor(Math.random() * pintaCard.length);
+  return pintaCard[pintaIndex];
 };
